f88b32781a5d1b5008e27734fbcbdf3cc6e194e9
Ibrohimjon.T <ibrohimjontursunov110@gmail.com>
2025-08-11 10:15:37 +0500

    fixed Scroll, MenuResponsive

--- src/components/Menu/MenuHero.jsx
+++ src/components/Menu/MenuHero.jsx
@@ -6,7 +6,7 @@ const MenuHero = () => {
 			{/* Text */}
 			<div className='flex gap-[32px] flex-col items-center justify-center'>
 				<div className='flex flex-col items-center gap-[12px]'>
-					<h1 className='font-calistoga font-[600] leading-[112%] text-8xl text-secondary w-[680px] h-[108px] text-center'>
+					<h1 className='font-calistoga font-[600] leading-[112%] text-8xl text-secondary w-[680px] h-[108px] text-center sm:text-[48px] md:text-[80px] md:leading-[112%] '>
 						Menu
 					</h1>
 					<p className='w-full h-[23px] text-center tracking-[2%] text-secondary font-cabin text-[18px] leading-[128%]'>
@@ -15,14 +15,14 @@ const MenuHero = () => {
 				</div>
 				{/* Button */}
 
-				<div className='flex flex-row items-center justify-center gap-3'>
+				<div className='flex flex-row items-center justify-center gap-3 sm:flex-col'>
 					<Button
 						text='Our Locations'
-						className='cursor-pointer px-4 py-3 bg-secondary text-primary rounded-3xl font-cabin'
+						className='cursor-pointer px-4 py-3 bg-secondary text-primary rounded-3xl font-cabin sm:w-full'
 					/>
 					<Button
 						text='About Us'
-						className='cursor-pointer px-4 py-3 rounded-3xl border-1 border-secondary text-secondary'
+						className='cursor-pointer px-4 py-3 rounded-3xl border-1 border-secondary text-secondary sm:w-full'
 					/>
 				</div>
 			</div>
--- src/components/ScrollTicker.jsx
+++ src/components/ScrollTicker.jsx
@@ -1,26 +1,16 @@
 import React from "react"
-// import { items } from "../constants"
-
-const items = [
-	{ icon: "/images/Smiley.png", text: "Great Coffee" },
-	// { icon: Bolt, text: "Fast Service" },
-	// { icon: Instagram, text: "Cozy Space" },
-	// { icon: Waterdrop, text: "Handcrafted Drinks" },
-	// { icon: Element, text: "Local Roasts" },
-	// { icon: Crown, text: "Global Flavor" },
-	// { icon: SmileyLove, text: "Friendly Baristas" },
-]
+import { items } from "../constants"
 
 const ScrollTicker = () => {
 	return (
-		<div className='bg-secondary overflow-hidden'>
+		<div className='bg-black overflow-hidden'>
 			<div className='flex animate-scroll gap-8 py-3'>
 				{items.map((item, index) => (
 					<div
 						key={index}
 						className='flex items-center gap-2 text-primary text-lg whitespace-nowrap'
 					>
-						<span className='text-xl'>{item.icon}</span>
+						<img src={item.icon} alt={item.text} className='text-primary' />
 						<span>{item.text}</span>
 					</div>
 				))}
